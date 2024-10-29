b77d331b5bae96741217e4574c439bb06cd50e45
Szymon Machnik <szymonmachnik07@gmail.com>
2024-10-29 22:16:04 +0100

    Added 3 sudoku to load to database

--- main.cpp
+++ main.cpp
@@ -22,300 +22,235 @@ void CleanupDeviceD3D();
 void ResetDevice();
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
+// Board
 const int BOARD_SIZE = 9;
 const int MAX_INPUT_LENGTH = 2;
-
-
 char board[BOARD_SIZE][BOARD_SIZE][MAX_INPUT_LENGTH];
-
-// void InitializeBoard(char (&board)[9][9][2]) {
-//     for (int i = 0; i < 9; ++i) {
-//         for (int j = 0; j < 9; ++j) {
-//             board[i][j][0] = '-';
-//             board[i][j][1] = '\0';
-//         }
-//     }
-// }
-
-// void resetBoard(char (&board)[9][9][2]) {
-//     for (int i = 0; i < 9; i++) {
-//         for (int j = 0; j < 9; j++) {
-//             board[i][j][0] = '-';
-//             board[i][j][1] = '\0'; 
-//         }
-//     }
-// }
-
-// void translateSolvedBoardIntoBoardToPrint(char (&boardToPrint)[9][9][2], vector<vector<char>> &solvedBoard) {
-//     for (int i = 0; i < 9; i++) {
-//         for (int j = 0; j < 9; j++) {
-//             boardToPrint[i][j][0] = solvedBoard[i][j];
-//             boardToPrint[i][j][1] = '\0'; 
-//         }
-//     }
-// }
-
-// void translateBoardToPrintIntoBoardToSolve(char (&boardToPrint)[9][9][2], vector<vector<char>> &boardToSolve, bool ableToSolve) {
-//     for (int i = 0; i < 9; i++) {
-//         vector<char> row;
-//         for (int j = 0; j < 9; j++) {
-//             if (boardToPrint[i][j][0] == '-') {
-//                 row.push_back('.');
-//             } else if (boardToPrint[i][j][0] - '0' >= 1 && boardToPrint[i][j][0] - '0' <= 9) {
-//                 row.push_back(boardToPrint[i][j][0]);
-//             } else if (boardToPrint[i][j][0] == '\0') {
-//                 row.push_back('.');
-//             } else {
-//                 ableToSolve = false;
-//                 break;
-//             }
-//         }
-//         boardToSolve.push_back(row);
-//     }
-// }
 
 
 // Main code
 int main(int, char**)
 {
-    WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
-    ::RegisterClassExW(&wc);
-    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"Dear ImGui DirectX9 Example", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
+  WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
+  ::RegisterClassExW(&wc);
+  HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"Dear ImGui DirectX9 Example", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
 
-    // Initialize Direct3D
-    if (!CreateDeviceD3D(hwnd)) {
-        CleanupDeviceD3D();
-        ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
-        return 1;
-    }
+  // Initialize Direct3D
+  if (!CreateDeviceD3D(hwnd)) {
+      CleanupDeviceD3D();
+      ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+      return 1;
+  }
 
-    // Show the window
-    ::ShowWindow(hwnd, SW_SHOWDEFAULT);
-    ::UpdateWindow(hwnd);
+  // Show the window
+  ::ShowWindow(hwnd, SW_SHOWDEFAULT);
+  ::UpdateWindow(hwnd);
 
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
 
-    // Setup Platform/Renderer backends
-    ImGui_ImplWin32_Init(hwnd);
-    ImGui_ImplDX9_Init(g_pd3dDevice);
+  // Setup Platform/Renderer backends
+  ImGui_ImplWin32_Init(hwnd);
+  ImGui_ImplDX9_Init(g_pd3dDevice);
 
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    io.Fonts->AddFontDefault();
-    io.FontGlobalScale = 3.7f;
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  io.Fonts->AddFontDefault();
+  io.FontGlobalScale = 3.7f;
 
-    ImGuiStyle& style = ImGui::GetStyle();
-    style.Colors[ImGuiCol_WindowBg] = ImVec4(153, 153, 153, 255);
+  ImGuiStyle& style = ImGui::GetStyle();
+  style.Colors[ImGuiCol_WindowBg] = ImVec4(153, 153, 153, 255);
 
-    
-    ImVec4 clear_color = ImVec4(130, 202, 237, 255);
-        
+  
+  ImVec4 clear_color = ImVec4(130, 202, 237, 255);
+      
 
-    SudokuSolver SudokuSolver;
-    // Main loop
-    bool done = false;
-    SudokuSolver.InitializeBoard(board);
-    while (!done)
+  SudokuSolver SudokuSolver;
+  SudokuSolver.InitializeBoard(board);
+
+  // Main loop
+  bool done = false;
+  while (!done)
+  {
+    MSG msg;
+    while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
     {
-        MSG msg;
-        while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
-        {
-            ::TranslateMessage(&msg);
-            ::DispatchMessage(&msg);
-            if (msg.message == WM_QUIT)
-                done = true;
-        }
-        if (done)
-            break;
+      ::TranslateMessage(&msg);
+      ::DispatchMessage(&msg);
+      if (msg.message == WM_QUIT)
+          done = true;
+    }
+    if (done)
+      break;
 
-        // Handle lost D3D9 device
-        if (g_DeviceLost)
-        {
-            HRESULT hr = g_pd3dDevice->TestCooperativeLevel();
-            if (hr == D3DERR_DEVICELOST)
-            {
-                ::Sleep(10);
-                continue;
-            }
-            if (hr == D3DERR_DEVICENOTRESET)
-                ResetDevice();
-            g_DeviceLost = false;
-        }
-
-        // Handle window resize (we don't resize directly in the WM_SIZE handler)
-        if (g_ResizeWidth != 0 && g_ResizeHeight != 0)
-        {
-            g_d3dpp.BackBufferWidth = g_ResizeWidth;
-            g_d3dpp.BackBufferHeight = g_ResizeHeight;
-            g_ResizeWidth = g_ResizeHeight = 0;
-            ResetDevice();
-        }
-
-        // Start the Dear ImGui frame
-        ImGui_ImplDX9_NewFrame();
-        ImGui_ImplWin32_NewFrame();
-        ImGui::NewFrame();
-
-        //Point Of Interest///////////////////////////////////////
-        
-        ImGui::SetNextWindowPos(ImVec2(368, 48));
-        ImGui::SetNextWindowSize(ImVec2(561, 561));
-        ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.5f, 0.5f, 0.5f, 1.0f));
-
-        if (ImGui::Begin("Board", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar)) {
-            int extraI = 0;
-            for (int i = 0; i < BOARD_SIZE; i++) {
-                if (i % 3 == 0 && i != 0) {
-                    extraI += 5;
-                }
-                int extraJ = 0;
-                for (int j = 0; j < BOARD_SIZE; j++) {
-                    if (j % 3 == 0 && j != 0) {
-                        extraJ += 5;
-                    }
-                    ImGui::SetCursorPos(ImVec2(8 + extraJ + j * 60, 8 + extraI + i * 60));
-                    ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
-                    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
-                    ImGui::SetNextItemWidth(55.0f);
-
-                    std::string id = "##" + std::to_string(i) + "_" + std::to_string(j);
-                    ImGui::InputText(id.c_str(), board[i][j], MAX_INPUT_LENGTH); 
-
-                    ImGui::PopStyleColor(2);
-                }
-            }
-        }
-        ImGui::End();
-        ImGui::PopStyleColor();
-
-        ImGui::SetNextWindowPos(ImVec2(368, 640));
-        ImGui::SetNextWindowSize(ImVec2(561, 75));
-        if (ImGui::Begin("Buttons", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar)) {
-            if (ImGui::Button("Load", ImVec2(176, 59))) {
-                SudokuSolver.loadSudoku(board);
-            }
-
-            ImGui::SameLine();
-            
-            if (ImGui::Button("Solve!", ImVec2(176, 59))) {
-                bool ableToSolve = true;
-                vector<vector<char>> boardToSolve;
-                SudokuSolver.translateBoardToPrintIntoBoardToSolve(board, boardToSolve, ableToSolve);
-                if (ableToSolve) {
-                    SudokuSolver.solveSudoku(boardToSolve);
-                    SudokuSolver.translateSolvedBoardIntoBoardToPrint(board, boardToSolve);
-                } else { 
-                    SudokuSolver.resetBoard(board);
-                }
-            }
-
-            // ImGui::SetCursorPos(ImVec2(8 + 10 + 6 * 60, 640));
-            // if (ImGui::Begin("ResetButton", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar)) {
-            //     if (ImGui::Button("Reset", ImVec2(210, 59))) {
-            //         SudokuSolver.resetBoard(board);
-            //     }
-
-            // }
-             // Ustawia przycisk "Reset" obok "Solve!"
-
-            // Przycisk "Reset"
-
-
-            ImGui::SameLine(); // Ustawia przycisk "Reset" obok "Solve!"
-
-            // Przycisk "Reset"
-            if (ImGui::Button("Reset", ImVec2(176, 59))) {
-                SudokuSolver.resetBoard(board);
-            }
-
-            
-
-        } 
-        ImGui::End();
-
-        // ImGui::SetNextWindowPos(ImVec2(704, 640));
-        // ImGui::SetNextWindowSize(ImVec2(225, 75));
-        // if (ImGui::Begin("ResetButton", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar)) {
-        //     if (ImGui::Button("Reset", ImVec2(210, 59))) {
-        //         SudokuSolver.resetBoard(board);
-        //     }
-
-        // } 
-        // ImGui::End();
-
-
-        // Rendering
-        ImGui::EndFrame();
-        g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
-        g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
-        g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
-
-        D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x*clear_color.w*255.0f), (int)(clear_color.y*clear_color.w*255.0f), (int)(clear_color.z*clear_color.w*255.0f), (int)(clear_color.w*255.0f));
-        g_pd3dDevice->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
-        if (g_pd3dDevice->BeginScene() >= 0)
-        {
-            ImGui::Render();
-            ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
-            g_pd3dDevice->EndScene();
-        }
-
-        HRESULT result = g_pd3dDevice->Present(nullptr, nullptr, nullptr, nullptr);
-        if (result == D3DERR_DEVICELOST)
-            g_DeviceLost = true;
+    // Handle lost D3D9 device
+    if (g_DeviceLost)
+    {
+      HRESULT hr = g_pd3dDevice->TestCooperativeLevel();
+      if (hr == D3DERR_DEVICELOST)
+      {
+        ::Sleep(10);
+        continue;
+      }
+      if (hr == D3DERR_DEVICENOTRESET)
+        ResetDevice();
+      g_DeviceLost = false;
     }
 
-    // Cleanup
-    ImGui_ImplDX9_Shutdown();
-    ImGui_ImplWin32_Shutdown();
-    ImGui::DestroyContext();
+    // Handle window resize (we don't resize directly in the WM_SIZE handler)
+    if (g_ResizeWidth != 0 && g_ResizeHeight != 0)
+    {
+      g_d3dpp.BackBufferWidth = g_ResizeWidth;
+      g_d3dpp.BackBufferHeight = g_ResizeHeight;
+      g_ResizeWidth = g_ResizeHeight = 0;
+      ResetDevice();
+    }
 
-    CleanupDeviceD3D();
-    ::DestroyWindow(hwnd);
-    ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+    // Start the Dear ImGui frame
+    ImGui_ImplDX9_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
 
-    return 0;
+    //Point Of Interest starts///////////////////////////////////////
+    
+    ImGui::SetNextWindowPos(ImVec2(368, 48));
+    ImGui::SetNextWindowSize(ImVec2(561, 561));
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.5f, 0.5f, 0.5f, 1.0f));
+
+    // Generate board
+    if (ImGui::Begin("Board", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar)) {
+      int extraI = 0;
+      for (int i = 0; i < BOARD_SIZE; i++) {
+        if (i % 3 == 0 && i != 0) {
+          extraI += 5;
+        }
+        int extraJ = 0;
+        for (int j = 0; j < BOARD_SIZE; j++) {
+          if (j % 3 == 0 && j != 0) {
+              extraJ += 5;
+          }
+          ImGui::SetCursorPos(ImVec2(8 + extraJ + j * 60, 8 + extraI + i * 60));
+          ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+          ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
+          ImGui::SetNextItemWidth(55.0f);
+
+          std::string id = "##" + std::to_string(i) + "_" + std::to_string(j);
+          ImGui::InputText(id.c_str(), board[i][j], MAX_INPUT_LENGTH); 
+
+          ImGui::PopStyleColor(2);
+        }
+      }
+    }
+    ImGui::End();
+    ImGui::PopStyleColor();
+
+    ImGui::SetNextWindowPos(ImVec2(368, 640));
+    ImGui::SetNextWindowSize(ImVec2(561, 75));
+    if (ImGui::Begin("Buttons", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar)) {
+
+      // Load button
+      if (ImGui::Button("Load", ImVec2(176, 59))) {
+          SudokuSolver.loadSudoku(board);
+      }
+
+      ImGui::SameLine();
+      
+      // Solve button
+      if (ImGui::Button("Solve!", ImVec2(176, 59))) {
+        bool ableToSolve = true;
+        vector<vector<char>> boardToSolve;
+        SudokuSolver.translateBoardToPrintIntoBoardToSolve(board, boardToSolve, ableToSolve);
+        if (ableToSolve) {
+          SudokuSolver.solveSudoku(boardToSolve);
+          SudokuSolver.translateSolvedBoardIntoBoardToPrint(board, boardToSolve);
+        } else { 
+          SudokuSolver.resetBoard(board);
+        }
+      }
+
+      ImGui::SameLine();
+
+      // Reset button
+      if (ImGui::Button("Reset", ImVec2(176, 59))) {
+        SudokuSolver.resetBoard(board);
+      }        
+
+    } 
+    ImGui::End();
+
+    //Point Of Interest ends///////////////////////////////////////
+
+    // Rendering
+    ImGui::EndFrame();
+    g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
+    g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+    g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
+
+    D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x*clear_color.w*255.0f), (int)(clear_color.y*clear_color.w*255.0f), (int)(clear_color.z*clear_color.w*255.0f), (int)(clear_color.w*255.0f));
+    g_pd3dDevice->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
+    if (g_pd3dDevice->BeginScene() >= 0)
+    {
+      ImGui::Render();
+      ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+      g_pd3dDevice->EndScene();
+    }
+
+    HRESULT result = g_pd3dDevice->Present(nullptr, nullptr, nullptr, nullptr);
+    if (result == D3DERR_DEVICELOST)
+      g_DeviceLost = true;
+  }
+
+  // Cleanup
+  ImGui_ImplDX9_Shutdown();
+  ImGui_ImplWin32_Shutdown();
+  ImGui::DestroyContext();
+
+  CleanupDeviceD3D();
+  ::DestroyWindow(hwnd);
+  ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+
+  return 0;
 }
 
 // Helper functions
 
 bool CreateDeviceD3D(HWND hWnd)
 {
-    if ((g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == nullptr)
-        return false;
+  if ((g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == nullptr)
+    return false;
 
-    // Create the D3DDevice
-    ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
-    g_d3dpp.Windowed = TRUE;
-    g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-    g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN; // Need to use an explicit format with alpha if needing per-pixel alpha composition.
-    g_d3dpp.EnableAutoDepthStencil = TRUE;
-    g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
-    g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;           // Present with vsync
-    //g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;   // Present without vsync, maximum unthrottled framerate
-    if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
-        return false;
+  // Create the D3DDevice
+  ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
+  g_d3dpp.Windowed = TRUE;
+  g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+  g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN; // Need to use an explicit format with alpha if needing per-pixel alpha composition.
+  g_d3dpp.EnableAutoDepthStencil = TRUE;
+  g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+  g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;           // Present with vsync
+  //g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;   // Present without vsync, maximum unthrottled framerate
+  if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
+    return false;
 
-    return true;
+  return true;
 }
 
 void CleanupDeviceD3D()
 {
-    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
-    if (g_pD3D) { g_pD3D->Release(); g_pD3D = nullptr; }
+  if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
+  if (g_pD3D) { g_pD3D->Release(); g_pD3D = nullptr; }
 }
 
 void ResetDevice()
 {
-    ImGui_ImplDX9_InvalidateDeviceObjects();
-    HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
-    if (hr == D3DERR_INVALIDCALL)
-        IM_ASSERT(0);
-    ImGui_ImplDX9_CreateDeviceObjects();
+  ImGui_ImplDX9_InvalidateDeviceObjects();
+  HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
+  if (hr == D3DERR_INVALIDCALL)
+    IM_ASSERT(0);
+  ImGui_ImplDX9_CreateDeviceObjects();
 }
 
 // Forward declare message handler from imgui_impl_win32.cpp
@@ -328,24 +263,24 @@ extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg
 // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
-        return true;
+  if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+    return true;
 
-    switch (msg)
-    {
-    case WM_SIZE:
-        if (wParam == SIZE_MINIMIZED)
-            return 0;
-        g_ResizeWidth = (UINT)LOWORD(lParam); // Queue resize
-        g_ResizeHeight = (UINT)HIWORD(lParam);
-        return 0;
-    case WM_SYSCOMMAND:
-        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
-            return 0;
-        break;
-    case WM_DESTROY:
-        ::PostQuitMessage(0);
-        return 0;
-    }
-    return ::DefWindowProcW(hWnd, msg, wParam, lParam);
+  switch (msg)
+  {
+  case WM_SIZE:
+    if (wParam == SIZE_MINIMIZED)
+      return 0;
+    g_ResizeWidth = (UINT)LOWORD(lParam); // Queue resize
+    g_ResizeHeight = (UINT)HIWORD(lParam);
+    return 0;
+  case WM_SYSCOMMAND:
+    if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+      return 0;
+    break;
+  case WM_DESTROY:
+    ::PostQuitMessage(0);
+    return 0;
+  }
+  return ::DefWindowProcW(hWnd, msg, wParam, lParam);
 }
--- sudokuHelperFunctions/sudoku.cpp
+++ sudokuHelperFunctions/sudoku.cpp
@@ -9,99 +9,99 @@ SudokuSolver::SudokuSolver() {
 // }
 
 bool SudokuSolver::isValid(int r, int c, char value, std::vector<std::vector<char>>& board) {
-    for (int i = 0; i < 9; i++) {
-        if (board[i][c] == value) return false;
-        if (board[r][i] == value) return false;
-        if (board[3 * (r / 3) + i / 3][3 * (c / 3) + (i % 3)] == value) return false;
-    }
-    return true;
+  for (int i = 0; i < 9; i++) {
+    if (board[i][c] == value) return false;
+    if (board[r][i] == value) return false;
+    if (board[3 * (r / 3) + i / 3][3 * (c / 3) + (i % 3)] == value) return false;
+  }
+  return true;
 }
 
 void SudokuSolver::backTrack(std::vector<std::vector<char>>& boardOriginal, int r, int c, bool& solved) {
-    if (solved) return;
-    if (r == 9 && c == 8) {
-        solved = true;
-        return;
-    }
-    if (r == 9) {
-        r = 0; 
-        c++;
-    }
+  if (solved) return;
+  if (r == 9 && c == 8) {
+    solved = true;
+    return;
+  }
+  if (r == 9) {
+    r = 0; 
+    c++;
+  }
 
-    if (boardOriginal[r][c] == '.') {
-        for (int i = 1; i < 10; i++) {
-            if (isValid(r, c, i + '0', boardOriginal)) {
-                char value = i + '0';
-                boardOriginal[r][c] = value;
+  if (boardOriginal[r][c] == '.') {
+    for (int i = 1; i < 10; i++) {
+      if (isValid(r, c, i + '0', boardOriginal)) {
+        char value = i + '0';
+        boardOriginal[r][c] = value;
 
-                backTrack(boardOriginal, r + 1, c, solved);
-
-                if (solved) return;
-
-                boardOriginal[r][c] = '.';
-            }
-        }
-    } else {
         backTrack(boardOriginal, r + 1, c, solved);
+
+        if (solved) return;
+
+        boardOriginal[r][c] = '.';
+      }
     }
+  } else {
+    backTrack(boardOriginal, r + 1, c, solved);
+  }
 }
 
 void SudokuSolver::solveSudoku(std::vector<std::vector<char>>& board) {
-    bool solved = false;
-    backTrack(board, 0, 0, solved);
-    return;
+  bool solved = false;
+  backTrack(board, 0, 0, solved);
+  return;
 }
 
 void SudokuSolver::InitializeBoard(char (&board)[9][9][2]) {
-    for (int i = 0; i < 9; ++i) {
-        for (int j = 0; j < 9; ++j) {
-            board[i][j][0] = '-';
-            board[i][j][1] = '\0';
-        }
+  for (int i = 0; i < 9; ++i) {
+    for (int j = 0; j < 9; ++j) {
+      board[i][j][0] = '-';
+      board[i][j][1] = '\0';
     }
+  }
 }
 
 void SudokuSolver::resetBoard(char (&board)[9][9][2]) {
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            board[i][j][0] = '-';
-            board[i][j][1] = '\0'; 
-        }
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      board[i][j][0] = '-';
+      board[i][j][1] = '\0'; 
     }
+  }
 }
 
 void SudokuSolver::translateSolvedBoardIntoBoardToPrint(char (&boardToPrint)[9][9][2], std::vector<std::vector<char>> &solvedBoard) {
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            boardToPrint[i][j][0] = solvedBoard[i][j];
-            boardToPrint[i][j][1] = '\0'; 
-        }
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      boardToPrint[i][j][0] = solvedBoard[i][j];
+      boardToPrint[i][j][1] = '\0'; 
     }
+  }
 }
 
 void SudokuSolver::translateBoardToPrintIntoBoardToSolve(char (&boardToPrint)[9][9][2], std::vector<std::vector<char>> &boardToSolve, bool &ableToSolve) {
-    for (int i = 0; i < 9; i++) {
-        std::vector<char> row;
-        for (int j = 0; j < 9; j++) {
-            if (boardToPrint[i][j][0] == '-') {
-                row.push_back('.');
-            } else if (boardToPrint[i][j][0] - '0' >= 1 && boardToPrint[i][j][0] - '0' <= 9) {
-                row.push_back(boardToPrint[i][j][0]);
-            } else if (boardToPrint[i][j][0] == '\0') {
-                row.push_back('.');
-            } else {
-                ableToSolve = false;
-                break;
-            }
-        }
-        boardToSolve.push_back(row);
+  for (int i = 0; i < 9; i++) {
+    std::vector<char> row;
+    for (int j = 0; j < 9; j++) {
+      if (boardToPrint[i][j][0] == '-') {
+        row.push_back('.');
+      } else if (boardToPrint[i][j][0] - '0' >= 1 && boardToPrint[i][j][0] - '0' <= 9) {
+        row.push_back(boardToPrint[i][j][0]);
+      } else if (boardToPrint[i][j][0] == '\0') {
+        row.push_back('.');
+      } else {
+        ableToSolve = false;
+        break;
+      }
     }
+    boardToSolve.push_back(row);
+  }
 }
 
 void SudokuSolver::loadSudoku(char (&board)[9][9][2]) {
-    std::ifstream inputFile;
+  std::ifstream inputFile;
 
-  std::string fileName = "sudokusToLoadData/1.txt";
+  std::string fileName = "sudokusToLoadData/3.txt";
 
   inputFile.open(fileName);
 
